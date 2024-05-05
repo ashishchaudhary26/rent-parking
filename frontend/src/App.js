a90014a004f7e8583167aaecb4ee828c88b732ec
ashishchaudhary26 <chaudharyash2002@gmail.com>
2024-05-05 23:51:18 +0530

    frontend started

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
